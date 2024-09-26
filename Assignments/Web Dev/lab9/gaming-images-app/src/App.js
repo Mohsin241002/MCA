4014ae0b8dfd85b5f4a262fcc450083a6d2e432f
Mohsin <chunawala246@gmail.com>
2024-09-26 23:35:24 +0530

    new

---
+++ Assignments/Web Dev/lab9/gaming-images-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GamingImages from './GamingImages';
+
+function App() {
+  return (
+    <div className="App">
+      <GamingImages />
+    </div>
+  );
+}
+
+export default App;
